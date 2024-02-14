666ff447397e94c36196c9f59027871606f02a1a
conx42 <97751032+conx42@users.noreply.github.com>
2024-02-14 17:30:42 +0530

    Introduction - Done

---
+++ Flutter & Dart - The Complete Guide [2024 Edition]/01_introduction.c
@@ -0,0 +1,7 @@
+/*
+Vid - 10
+*/
+About - Material Design 
+    1. flutter app By default use 'Material Design'
+    -> which is 'google''s' flexible design system.
+    2. In the end the 'Material Design' thing is simply a set of suggestion, rules & guidelines established by google that helps me building beautiful user interfaces.
